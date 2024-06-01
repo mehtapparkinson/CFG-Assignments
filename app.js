223d3186d3ad95ee61337cc3d474b4031fd06124
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-06-01 13:12:02 +0300

    DOM elements created

---
+++ app.js
@@ -0,0 +1,10 @@
+const inputDescription = document.getElementById('description');
+const inputAmount = document.getElementById('amount');
+const typeInput = document.getElementById('type');  
+const addButton = document.getElementById('add-button')
+const totalIncome = document.querySelector('.total-income');
+const totalExpenses = document.querySelector('.total-expenses');
+const transactions = document.querySelector('.transactions');
+
+
+let transactionsArray = [];
