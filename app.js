ae76651ddce385e506f47c9249ff774e12441885
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-06-01 14:20:30 +0300

    showing transctions list added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const addButton = document.getElementById("add-button");
 const message = document.getElementById("message");
 const totalIncome = document.querySelector(".total-income");
 const totalExpenses = document.querySelector(".total-expenses");
-const transactions = document.querySelector(".transactions");
+const transactionsContainer = document.querySelector(".transactions");
 
 let transactionsArray = [];
 
@@ -16,15 +16,31 @@ function addTransaction() {
   const amount = parseInt(inputAmount.value.trim());
   const type = inputType.value;
   if (description && amount && type) {
+    message.textContent = "";
     const transaction = {
       description,
       amount,
       type,
     };
     transactionsArray.push(transaction);
+    console.log(transactionsArray);
+    showTransactions();
   } else {
     message.textContent = "";
     const failedMessage = "Please fill all fields!";
     message.textContent = failedMessage;
+    message.style.color = 'red';
+    message.style.fontWeight = '800';
   }
+}
+
+function showTransactions () {
+  transactionsContainer.textContent = '';
+  transactionsArray.forEach(element => {
+    const transactionElement = document.createElement('p');
+    transactionElement.classList.add('transaction');
+    transactionElement.textContent = `${element.description}  -------------- ${element.amount}`
+     transactionElement.style.color = element.type === 'income' ? 'green' : 'red';
+    transactionsContainer.appendChild(transactionElement);
+  });
 }
