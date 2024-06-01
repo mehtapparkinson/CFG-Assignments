76d7a90b6d7b4f9fcc84332854c5e1d81c478271
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-06-01 13:54:18 +0300

    addTransaction function, input validation added

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const inputDescription = document.getElementById('description');
-const inputAmount = document.getElementById('amount');
-const typeInput = document.getElementById('type');  
-const addButton = document.getElementById('add-button')
-const totalIncome = document.querySelector('.total-income');
-const totalExpenses = document.querySelector('.total-expenses');
-const transactions = document.querySelector('.transactions');
-
+const inputsContainer = document.querySelector(".inputs-container");
+const inputDescription = document.getElementById("description");
+const inputAmount = document.getElementById("amount");
+const inputType = document.getElementById("type");
+const addButton = document.getElementById("add-button");
+const message = document.getElementById("message");
+const totalIncome = document.querySelector(".total-income");
+const totalExpenses = document.querySelector(".total-expenses");
+const transactions = document.querySelector(".transactions");
 
 let transactionsArray = [];
+
+addButton.addEventListener("click", addTransaction);
+function addTransaction() {
+  const description = inputDescription.value.trim();
+  const amount = parseInt(inputAmount.value.trim());
+  const type = inputType.value;
+  if (description && amount && type) {
+    const transaction = {
+      description,
+      amount,
+      type,
+    };
+    transactionsArray.push(transaction);
+  } else {
+    message.textContent = "";
+    const failedMessage = "Please fill all fields!";
+    message.textContent = failedMessage;
+  }
+}
